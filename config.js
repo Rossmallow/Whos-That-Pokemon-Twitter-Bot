05bf6612990780f0a5b7e4b59032ec631d6bb018
Ross Nelson <Rossn430@gmail.com>
2017-08-07 19:38:40 -0500

    changed keys in config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {
-  consumer_key:         'TSl9GNVGHHECeVXXlHoum5wbB',
-  consumer_secret:      'Y550jnlzj9Mt2UDWU380Y3ycxSpnKV0VS1wcw3CmqguapPErXy',
-  access_token:         '857771956053000192-ToZ7GaAPbVdDyLXSZ2AhDbjBwERqU3L',
-  access_token_secret:  'drHg6j0ohgrEqPhkbJTMbME3wbliJwSXg2vY2WS80UeU0'
+  consumer_key:         'KMbIaxM8ZvPw5JYnz54axgHgR',
+  consumer_secret:      '3KE4XvBDOyuChzW0kO5mNpeEQPbIazdbHYipVMywDrrQFmaHnN',
+  access_token:         '857771956053000192-PQmV3N1uYK8i6TgNay6AIUw59N96oSN',
+  access_token_secret:  'owdL98p1gMnBvs5sCVbs2bJvHxpnJeKIyFT6HprO7DQEV'
 }
 
 module.exports = config;
